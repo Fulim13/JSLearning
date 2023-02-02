2d0e367aafe4dfc4697511908ce73a0299dd3898
Fulim13 <87457764+Fulim13@users.noreply.github.com>
2023-02-02 17:35:18 +0800

    until variable

---
+++ variable.js
@@ -0,0 +1,26 @@
+//Variable
+//a box(label) can store value
+//can reference again and again
+let myName = "Wong Fu Lim";
+console.log(myName); //Wong Fu Lim
+console.log(myName); //Wong Fu Lim
+
+// Naming Convention - camelCasing
+
+// cannot start with num
+// let 3num = "Love"
+
+// varname can start with alp,$,_
+let a = "b";
+let $a = "b";
+let _a = "b";
+
+// cannnot use reserved keyword like name, function and new
+// let new = "b"
+
+// cannot redeclare same variable name , but can chagne the variable value
+// let love = "L"
+// let love = "A"
+
+// CONSTANT - all upper case
+let PI = 3.1415;
